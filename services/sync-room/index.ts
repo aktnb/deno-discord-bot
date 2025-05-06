1b3673232c6b6a49a287b31c6519c739975c1ce8
aktnb <70847394+aktnb@users.noreply.github.com>
2025-05-06 12:00:17 +0900

    format

--- services/sync-room/index.ts
+++ services/sync-room/index.ts
@@ -52,7 +52,7 @@ export async function syncRoom(voiceChannel: VoiceChannel) {
     if (!textChannel) {
       console.log(
         "Creating text channel for voice channel:",
-        voiceChannel.name
+        voiceChannel.name,
       );
 
       const permissionOverwrites: OverwriteResolvable[] = voiceChannel.members
@@ -75,7 +75,8 @@ export async function syncRoom(voiceChannel: VoiceChannel) {
 
       try {
         await textChannel.send({
-          content: `このチャンネルは${voiceChannel.url}専用のテキストチャンネルです.\n${voiceChannel.url}に参加しているメンバーだけがこのチャンネルを見ることができます.`,
+          content:
+            `このチャンネルは${voiceChannel.url}専用のテキストチャンネルです.\n${voiceChannel.url}に参加しているメンバーだけがこのチャンネルを見ることができます.`,
         });
       } catch (_) {
         console.error("Error sending message to text channel:");
@@ -83,7 +84,7 @@ export async function syncRoom(voiceChannel: VoiceChannel) {
     }
 
     console.log(
-      `Syncing voice channel and text channel:${textChannel.name}:${guild.name}`
+      `Syncing voice channel and text channel:${textChannel.name}:${guild.name}`,
     );
 
     await syncMember(voiceChannel, textChannel);
@@ -109,7 +110,7 @@ export async function syncRoom(voiceChannel: VoiceChannel) {
 
 async function syncMember(
   voiceChannel: VoiceChannel,
-  textChannel: TextChannel
+  textChannel: TextChannel,
 ) {
   await Promise.all(
     voiceChannel.members
@@ -120,14 +121,14 @@ async function syncMember(
           ViewChannel: true,
           ReadMessageHistory: true,
         })
-      )
+      ),
   );
 
   await Promise.all(
     textChannel.members
       .filter((m) => !m.user.bot)
       .filter((m) => !voiceChannel.members.has(m.id))
-      .map((m) => textChannel.permissionOverwrites.delete(m))
+      .map((m) => textChannel.permissionOverwrites.delete(m)),
   );
 }
 
